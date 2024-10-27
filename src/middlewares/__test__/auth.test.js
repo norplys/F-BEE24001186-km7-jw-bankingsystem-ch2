e973c2a2398b8546f9932275d518883154cfd6db
norplys <joywinata88@gmail.com>
2024-10-27 21:53:56 +0700

    fix: fixing json web token error

--- src/middlewares/__test__/auth.test.js
+++ src/middlewares/__test__/auth.test.js
@@ -1,150 +1,183 @@
-import { describe, jest } from "@jest/globals";
+import { afterEach, describe, it, jest } from "@jest/globals";
 
 const mockCreateUser = jest.fn();
 const mockGetAllUser = jest.fn();
 const mockGetUserById = jest.fn();
-const mockGetUserByEmail = jest.fn(); 
+const mockGetUserByEmail = jest.fn();
 
 jest.unstable_mockModule("../../services/user.js", () => ({
-    UserService: jest.fn().mockImplementation(() => ({
-        createUser: mockCreateUser,
-        getAllUser: mockGetAllUser,
-        getUserById: mockGetUserById,
-        getUserByEmail: mockGetUserByEmail,
-    })),
+  UserService: jest.fn().mockImplementation(() => ({
+    createUser: mockCreateUser,
+    getAllUser: mockGetAllUser,
+    getUserById: mockGetUserById,
+    getUserByEmail: mockGetUserByEmail,
+  })),
 }));
 
-jest.unstable_mockModule("jsonwebtoken",
-    () => ({
-        default: {
-            verify: jest.fn(),
-        }
-    })
-);
+jest.unstable_mockModule("jsonwebtoken", () => ({
+  default: {
+    verify: jest.fn(),
+    JsonWebTokenError: class JsonWebTokenError extends Error {},
+  },
+}));
 
 const jwt = await import("jsonwebtoken");
 
 const userMiddleware = await import("../auth.js");
 
 describe("userMiddleware", () => {
-    describe("isAuthorized", () => {
-        it("should call next if user is authorized", async () => {
-            const mockRequest = {
-                get: jest.fn().mockReturnValue("Bearer token"),
-            };
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
 
-            const mockResponse = {
-                status: jest.fn().mockReturnThis(),
-                json: jest.fn(),
-                locals: {},
-            };
+  describe("isAuthorized", () => {
+    it("should call next if user is authorized", async () => {
+      const mockRequest = {
+        get: jest.fn().mockReturnValue("Bearer token"),
+      };
 
-            const mockNext = jest.fn();
+      const mockResponse = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+        locals: {},
+      };
 
-            const user = {
-                id: 1,
-            };
+      const mockNext = jest.fn();
 
-            mockGetUserById.mockResolvedValue(user);
-            jwt.default.verify.mockReturnValue({ id: 1 });
+      const user = {
+        id: 1,
+      };
 
-            await userMiddleware.isAuthorized(mockRequest, mockResponse, mockNext);
+      mockGetUserById.mockResolvedValue(user);
+      jwt.default.verify.mockReturnValue({ id: 1 });
 
-            expect(mockResponse.locals.user).toEqual(user);
-            expect(mockNext).toHaveBeenCalled();
-        });
+      await userMiddleware.isAuthorized(mockRequest, mockResponse, mockNext);
 
-        it("should return 401 if authorization header is missing", async () => {
-            const mockRequest = {
-                get: jest.fn(),
-            };
-
-            const mockResponse = {
-                status: jest.fn().mockReturnThis(),
-                json: jest.fn(),
-                locals: {},
-            };
-
-            const mockNext = jest.fn();
-
-            await userMiddleware.isAuthorized(mockRequest, mockResponse, mockNext);
-
-            expect(mockResponse.status).toHaveBeenCalledWith(401);
-            expect(mockResponse.json).toHaveBeenCalledWith({ message: "Missing authorization header" });
-            expect(mockNext).not.toHaveBeenCalled();
-        });
+      expect(mockResponse.locals.user).toEqual(user);
+      expect(mockNext).toHaveBeenCalled();
     });
 
-    it("should return 401 if authorization type is invalid", async () => {
-        const mockRequest = {
-            get: jest.fn().mockReturnValue("Invalid token"),
-        };
+    it("should return 401 if authorization header is missing", async () => {
+      const mockRequest = {
+        get: jest.fn(),
+      };
 
-        const mockResponse = {
-            status: jest.fn().mockReturnThis(),
-            json: jest.fn(),
-            locals: {},
-        };
+      const mockResponse = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+        locals: {},
+      };
 
-        const mockNext = jest.fn();
+      const mockNext = jest.fn();
 
-        await userMiddleware.isAuthorized(mockRequest, mockResponse, mockNext);
+      await userMiddleware.isAuthorized(mockRequest, mockResponse, mockNext);
 
-        expect(mockResponse.status).toHaveBeenCalledWith(401);
-        expect(mockResponse.json).toHaveBeenCalledWith({ message: "Invalid authorization type" });
-        expect(mockNext).not.toHaveBeenCalled();
+      expect(mockResponse.status).toHaveBeenCalledWith(401);
+      expect(mockResponse.json).toHaveBeenCalledWith({
+        message: "Missing authorization header",
+      });
+      expect(mockNext).not.toHaveBeenCalled();
+    });
+  });
+
+  it("should return 401 if authorization type is invalid", async () => {
+    const mockRequest = {
+      get: jest.fn().mockReturnValue("Invalid token"),
+    };
+
+    const mockResponse = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+      locals: {},
+    };
+
+    const mockNext = jest.fn();
+
+    await userMiddleware.isAuthorized(mockRequest, mockResponse, mockNext);
+
+    expect(mockResponse.status).toHaveBeenCalledWith(401);
+    expect(mockResponse.json).toHaveBeenCalledWith({
+      message: "Invalid authorization type",
+    });
+    expect(mockNext).not.toHaveBeenCalled();
+  });
+
+  it("should return 404 if user is not found", async () => {
+    const mockRequest = {
+      get: jest.fn().mockReturnValue("Bearer token"),
+    };
+
+    const mockResponse = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+      locals: {},
+    };
+
+    const mockNext = jest.fn();
+
+    mockGetUserById.mockResolvedValue(null);
+    jwt.default.verify.mockReturnValue({ id: 1 });
+
+    await userMiddleware.isAuthorized(mockRequest, mockResponse, mockNext);
+
+    expect(mockResponse.status).toHaveBeenCalledWith(404);
+    expect(mockResponse.json).toHaveBeenCalledWith({
+      message: "User not found",
+    });
+    expect(mockNext).not.toHaveBeenCalled();
+  });
+
+  it("should return 401 if token is invalid", async () => {
+    const mockRequest = {
+      get: jest.fn().mockReturnValue("Bearer token"),
+    };
+
+    const mockResponse = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+      locals: {},
+    };
+
+    const mockNext = jest.fn();
+
+    mockGetUserById.mockResolvedValue(null);
+    jwt.default.verify.mockImplementation(() => {
+      throw new jwt.default.JsonWebTokenError("Invalid token");
+    });
+    await userMiddleware.isAuthorized(mockRequest, mockResponse, mockNext);
+
+    expect(mockResponse.status).toHaveBeenCalledWith(401);
+    expect(mockResponse.json).toHaveBeenCalledWith({
+      message: "Invalid token",
+    });
+    expect(mockNext).not.toHaveBeenCalled();
+  });
+
+  it("should return 500 if an error occurs", async () => {
+    const mockRequest = {
+      get: jest.fn().mockReturnValue("Bearer token"),
+    };
+
+    const mockResponse = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+      locals: {},
+    };
+
+    const mockNext = jest.fn();
+
+    mockGetUserById.mockResolvedValue(null);
+
+    jwt.default.verify.mockImplementation(() => {
+      throw new Error("Internal server error");
     });
 
-    it("should return 404 if user is not found", async () => {
-        const mockRequest = {
-            get: jest.fn().mockReturnValue("Bearer token"),
-        };
+    await userMiddleware.isAuthorized(mockRequest, mockResponse, mockNext);
 
-        const mockResponse = {
-            status: jest.fn().mockReturnThis(),
-            json: jest.fn(),
-            locals: {},
-        };
-
-        const mockNext = jest.fn();
-
-        mockGetUserById.mockResolvedValue(null);
-        jwt.default.verify.mockReturnValue({ id: 1 });
-
-        await userMiddleware.isAuthorized(mockRequest, mockResponse, mockNext);
-
-        expect(mockResponse.status).toHaveBeenCalledWith(404);
-        expect(mockResponse.json).toHaveBeenCalledWith({ message: "User not found" });
-        expect(mockNext).not.toHaveBeenCalled();
+    expect(mockResponse.status).toHaveBeenCalledWith(500);
+    expect(mockResponse.json).toHaveBeenCalledWith({
+      message: "Internal server error",
     });
-
-    // it("should return 401 if token is invalid", async () => {
-    //     const mockRequest = {
-    //         get: jest.fn().mockReturnValue("Bearer token"),
-    //     };
-
-    //     const mockResponse = {
-    //         status: jest.fn().mockReturnThis(),
-    //         json: jest.fn(),
-    //         locals: {},
-    //     };
-
-    //     const mockNext = jest.fn();
-
-    //     mockGetUserById.mockResolvedValue(null);
-    //     jwt.default.verify.mockImplementation(() => {
-    //         throw new Error("Invalid token");
-    //     });
-    //     await userMiddleware.isAuthorized(mockRequest, mockResponse, mockNext);
-
-    //     expect(mockResponse.status).toHaveBeenCalledWith(401);
-    //     expect(mockResponse.json).toHaveBeenCalledWith({ message: "Invalid token" });
-    //     expect(mockNext).not.toHaveBeenCalled();
-    // });
-
+    expect(mockNext).not.toHaveBeenCalled();
+  });
 });
-
-
-    
-
-
