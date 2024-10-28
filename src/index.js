3c914997be3242e5efba054764e5b5f18ab49975
norplys <joywinata88@gmail.com>
2024-10-28 12:09:36 +0700

    feat: add swagger documentation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express, { json } from "express";
 import routes from "./routes/index.js";
 import swaggerUi from "swagger-ui-express";
-import swaggerDocument from "./swagger_output.json" assert {type: "json"};
+import swaggerDocument from "./swagger.json" assert {type: "json"};
 
 async function main() {
   const app = express();
@@ -15,7 +15,8 @@ async function main() {
   app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
   app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port http://localhost:${port}`);
+    console.log(`API documentation is available at http://localhost:${port}/api-docs`);
   });
 }
 
