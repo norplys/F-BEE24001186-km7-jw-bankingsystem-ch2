f4fc09c74b03b125ff9823963d4f040a5ad42089
norplys <joywinata88@gmail.com>
2024-11-07 19:59:30 +0700

    fix: fixing async upload

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -2,55 +2,42 @@ import { uploadToMemory } from "../utils/multer.js";
 import { imageKit } from "../utils/imageKit.js";
 import crypto from "crypto";
 
-export function parseImage(
-    req,
-    res,
-    next
-) {
-    uploadToMemory(req, res, (err) => {
-        if (err) {
-            res.status(400).send({
-                message: "Error uploading image",
-            });
-        } else {
-            next();
-        }
-    });
+export function parseImage(req, res, next) {
+  uploadToMemory(req, res, (err) => {
+    if (err) {
+      res.status(400).send({
+        message: "Error uploading image",
+      });
+    } else {
+      next();
+    }
+  });
 }
 
-export function uploadToImageKit(
-    req,
-    res,
-    next
-) {
+export async function uploadToImageKit(req, res, next) {
+  try {
     const file = req.file;
-    
+
     if (!file) {
-        res.status(400).send({
-            message: "No image uploaded",
-        });
-        return;
+      res.status(400).send({
+        message: "No image uploaded",
+      });
+      return;
     }
-    
-    const fileExtension = file.originalname.split('.').pop();
+
+    const fileExtension = file.originalname.split(".").pop();
     const fileName = `${crypto.randomUUID()}.${fileExtension}`;
 
-    imageKit.upload(
-        {
-            file: file.buffer.toString("base64"),
-            fileName
-        },
-        (err, result) => {
-            if (err) {
-                res.status(400).send({
-                    message: "Error uploading image",
-                });
-            } else {
-                res.locals.imageUrl = result.url;
-                next();
-            }
-        }
-    );
-
+    const result = await imageKit.upload({
+      file: file.buffer,
+      fileName,
+    });
     
+    res.locals.imageUrl = result.url;
+    next();
+  } catch (error) {
+    res.status(500).send({
+      message: "Error uploading image",
+    });
+  }
 }
